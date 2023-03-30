364ede5ffeff8b7f8f13b6dada2f413f08d331bb
ani1999deep <anideepbhowmick1@gmail.com>
2023-03-30 17:25:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+//Url of the files
+const PNG_FILE_URL = `http://localhost:3000/react_tutorial.png`
+const PDF_FILE_URL = `http://localhost:3000/react_tutorial.pdf`
+const CSV_FILE_URL = `http://localhost:3000/react_tutorial.csv`
+const ZIP_FILE_URL = `http://localhost:3000/react_tutorial.zip`
 function App() {
+  //Button onClick function click 
+  const downloadFileAtURL = (url) => {
+    //Get the file name part
+    const fileName = url.split('/').pop()
+    //**********************************
+    const aTag = document.createElement("a");
+    aTag.href = url
+    aTag.setAttribute(`download`, fileName)
+    document.body.appendChild(aTag)
+    aTag.click()
+    aTag.remove();
+
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={() => { downloadFileAtURL(PNG_FILE_URL) }}>Download PNG button</button>
+      <button onClick={() => { downloadFileAtURL(PDF_FILE_URL) }}>Download PDF button</button>
+      <button onClick={() => { downloadFileAtURL(CSV_FILE_URL) }}>Download CSV button</button>
+      <button onClick={() => { downloadFileAtURL(ZIP_FILE_URL) }}>Download ZIP button</button>
+
+
     </div>
   );
 }
